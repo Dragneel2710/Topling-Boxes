78610a77f5cafe0812746cfe91f36df9785ac448
Dragneel2710 <61239842+Dragneel2710@users.noreply.github.com>
2020-03-11 20:19:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box,box2,box3;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+    box = new Box(200,300,50,50);
+    box2 = new Box(240,100,50,100);
+    box3 = new Ground(200,390,400,20);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box.display();
+    box2.display();
+    box3.display();
+}
